e12297941ee630afe4e5acec9600c1ce1c043dc9
A Egorov on work <mymailegorov@gmail.com>
2020-04-01 17:10:54 +0300

    add project data

---
+++ TransferAssetList/Command.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace TransferAssetList
+{
+	[TransactionAttribute(TransactionMode.Manual)]
+	[RegenerationAttribute(RegenerationOption.Manual)]
+	public class Command : IExternalCommand
+	{
+		public Result Execute(ExternalCommandData commandData,
+			ref string messege,
+			ElementSet elements)
+		{
+			RevitManager revitManager = new RevitManager(commandData);
+
+			return Result.Succeeded;
+		}
+	}
+}
---
+++ TransferAssetList/RevitManager.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace TransferAssetList
+{
+	class RevitManager
+	{
+		public static UIApplication uiApplication;
+		public static Application app;
+		public static UIDocument uiDoc;
+		public static Document doc;
+		public static WorksetTable worksetTable;
+
+		public RevitManager(ExternalCommandData commandData)
+		{
+			uiApplication = commandData.Application;
+			app = uiApplication.Application;
+			uiDoc = uiApplication.ActiveUIDocument;
+			doc = uiDoc.Document;
+			worksetTable = doc.GetWorksetTable();
+		}
+	}
+}
